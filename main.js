0a23b02ce5eb8e59a60868239180c56842c2e17a
yuki1201 <yuki12017825@gmail.com>
2022-10-15 14:26:40 +0900

    add other shop

--- main.js
+++ main.js
@@ -39,18 +39,49 @@ function getRandomInt(max) {
 }
 
 function makecard(menu){
-  return '<div class="item__Card-sc-2teu2n-1 cKscZI"><h4 class="item__OrderLabel-sc-2teu2n-0 kEsMVQ ribbon">'+menu[0]+'</h4><h2 class="item__MenuName-sc-2teu2n-2 dpqSil">'+menu[1]+'</h2><p>'+menu[2]+'円 '+menu[3]+'kcal 塩分 '+menu[7]+'g</p></div>'
+  return '<a href="'+menu[13]+'"target="_blank"><div class="item__Card-sc-2teu2n-1 cKscZI"><h4 class="item__OrderLabel-sc-2teu2n-0 kEsMVQ ribbon">'+menu[0]+'</h4><h2 class="item__MenuName-sc-2teu2n-2 dpqSil">'+menu[1]+'</h2><p>'+menu[2]+'円 '+menu[3]+'kcal 塩分 '+menu[7]+'g</p></div></a>'
 }
 
 function makeresult(vcs,rgy){
   return '<div class="iteml"><h2>合計</h2></div><div class="itemr"><h3>'+vcs[0]+'円</br>'+vcs[1]+'kcal</br>塩分相当量'+vcs[2]+'g</br>赤：'+rgy[0].toFixed(1)+'点</br>緑：'+rgy[1].toFixed(1)+'点</br>黄：'+rgy[2].toFixed(1)+'点</h3></div></div>'
 }
 
+function selectshop(ddmenu){
+  switch(ddmenu){
+    case '0':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv');
+    break;
+    case '1':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vQYpyRZCLhMy_9GpTtDY_EGeyusR18Td38iunmnykoRMgCFLnzLU16cH6VnwUbPUplTzZZZ93GNJRa_/pub?gid=0&single=true&output=csv');
+    break;
+    case '2':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRJcC1CZd_5YmWM75pLIsix8HjbVy1BjSA81S5ZvKAsjCxSa8KzYnYL9YHOeMrgp_Cwu-f94cco8C9z/pub?gid=0&single=true&output=csv');
+    break;
+    case '3':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vQDAciFaCDY7fp16T8cBEhYPqcJxArUntqKmmKTH3E8TwfeW_JKlG9InQUa4HE_hSjFsAbzD40Tel00/pub?gid=0&single=true&output=csv');
+    break;
+    case '4':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vSdctUU_yebJ_vqUrv84ZcGOJuPm9gD3BleU3U8iy84mS5itiQ40UgXO8F3Z7Pb6wRWjgmhtZEA45pn/pub?gid=0&single=true&output=csv');
+    break;
+    case '5':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vTq7Sw06BPSG2Ha9ZBNAWiQRMtyi_CWkYnRdnQQh6Y_lx2rJpLql0q_Jyhma83L9oOtqcJ8qT_ASWJG/pub?gid=0&single=true&output=csv');
+    break;
+    case '6':
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vR2_aY7gZIoEpAPBLmXNr2Ryz3AFD43hPd22gAFjOzsJkvHa1RxQ9dz5QOXiS1VA_ZZp4WqBFXNiesO/pub?gid=0&single=true&output=csv');
+    break;
+    default:
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv');
+  }
+  return data
+}
+
 function onButtonClick() {
   document.getElementById("output_message").innerHTML=''
   document.getElementById("result").innerHTML=''
   document.getElementById("note").innerHTML=''
-    var data = csvToArray("https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv");
+  let ddmenu = document.getElementById('shoplist').value;
+  console.log(ddmenu);
+    var data = selectshop(ddmenu);
     //console.log(datasort(data,11));
     data=arrayShuffle(data);
     var value=0;

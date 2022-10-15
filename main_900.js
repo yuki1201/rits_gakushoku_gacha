26b457b2fb0137f46a6f43be1afb6bafb4373899
Cork0158 <59688657+Cork0158@users.noreply.github.com>
2022-10-15 20:07:20 +0900

    [fix] 「結果をツイート」の表示の軽微な変更

--- main_900.js
+++ main_900.js
@@ -119,7 +119,7 @@ function tweet() {
     tw_contents=tw_contents+"%0a・"+gachalist[i][1];
   }
 
-  tw_contents=tw_contents+"%0a%0a合計："+vcs[0]+"円"+vcs[1]+"kcal%0a"
+  tw_contents=tw_contents + "%0a%0a合計：" + vcs[0] + "円 (" + vcs[1] + "kcal)%0a";
   tw_contents=tw_contents+"%0a%20%23立命館学食ガチャ%20%0a"
   var url = "https://yuki1201.github.io/rits_gakushoku_gacha/";
   window.open().location.href = ("https://twitter.com/share?url=" + url + "&text=" + tw_contents + "&count=none&lang=ja");
